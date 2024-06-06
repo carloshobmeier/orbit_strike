0fb02d233db3a9e85a0eaaf04ddb3bbc28031d08
carloshobmeier <carloshobmeier@hotmail.com>
2024-06-06 17:44:04 -0300

    final

--- javascripts/script.js
+++ javascripts/script.js
@@ -57,6 +57,10 @@ let direcaoHorizontal = 0;
 let direcaoVertical = 0;
 
 
+let intervaloCriarNaveVida;
+let intervaloChecarColisaoItemVida;
+
+
 const atualizarPontos = (pontos) => {
   pontos.textContent = `Pontos: ${pontosAtual.toLocaleString()}`;
 }
@@ -299,9 +303,10 @@ const moveNaveInimigas = () => {
 const colisao = () => {
   const todasNavesInimigas = document.querySelectorAll(".inimigo, .chefe");
   const todosTiros = document.querySelectorAll(".tiro");
+  const todasNavesVida = document.querySelectorAll(".nave-vida"); // Seleciona também as naves de vida
 
-  todasNavesInimigas.forEach((naveInimiga) => {
-    todosTiros.forEach((tiro) => {
+  todasNavesInimigas.forEach(naveInimiga => {
+    todosTiros.forEach(tiro => {
       const colisaoNaveInimiga = naveInimiga.getBoundingClientRect();
       const colisaoTiro = tiro.getBoundingClientRect();
       if (
@@ -322,18 +327,35 @@ const colisao = () => {
                                 naveInimiga.classList.contains('rapida') ? 100 : 10;
           pontosAtual += pontosPorNave;
           atualizarPontos(pontos);
-
-
+          
           const isChefe = naveInimiga.classList.contains('chefe');
           naveInimigaDestruida(posicaoLeftNaveInimiga, posicaoTopNaveInimiga, isChefe);
-          naveInimiga.remove();  // Remove a nave inimiga do DOM
+          naveInimiga.remove();
         } else {
           naveInimiga.setAttribute("data-vida", vidaAtualNaveInimiga);
         }
       }
     });
   });
+
+  // Colisão com a nave de vida
+  todasNavesVida.forEach(naveVida => {
+    todosTiros.forEach(tiro => {
+      const colisaoNaveVida = naveVida.getBoundingClientRect();
+      const colisaoTiro = tiro.getBoundingClientRect();
+      if (
+        colisaoNaveVida.left < colisaoTiro.right &&
+        colisaoNaveVida.right > colisaoTiro.left &&
+        colisaoNaveVida.top < colisaoTiro.bottom &&
+        colisaoNaveVida.bottom > colisaoTiro.top
+      ) {
+        tiro.remove();  // Remove o tiro
+        destruirNaveVida(naveVida);  // Trata a colisão com a nave de vida
+      }
+    });
+  });
 }
+
 
 
 
@@ -397,36 +419,133 @@ const audioPassou = () => {
 }
 
 
-function pausarJogo() {
-  if (!jogoPausado) {
-    jogoPausado = true;
-    // Limpar todos os intervalos para evitar duplicações
-    clearInterval(checaMoveNave);
-    clearInterval(checaMoveTiros);
-    clearInterval(checaMoveNaveInimigas);
-    clearInterval(checaNaveInimigas);
-    clearInterval(checaNaveInimigasRapidas);
-    clearInterval(checaColisao);
-    clearInterval(checaTiros);
-    document.getElementById('botaoPausa').style.display = 'block';
-  }
+
+const criarNaveVida = () => {
+  const naveVida = document.createElement('div');
+  naveVida.className = 'nave-vida';
+  naveVida.style.position = 'absolute';
+  naveVida.style.width = '100px';
+  naveVida.style.height = '100px';
+  naveVida.style.backgroundImage = "url('/imagens/pod_vida.png')";
+  naveVida.style.backgroundSize = 'cover';
+  naveVida.style.left = '0px';
+  naveVida.style.top = '20px'; // Posiciona no topo do cenário
+  cenario.appendChild(naveVida);
+  moverNaveVida(naveVida);
+}
+
+const intervalosMovimentoNavesVida = [];
+const intervalosMovimentoItensVida = [];
+
+const moverNaveVida = (naveVida) => {
+  let direcao = 1; // Define a direção inicial para a direita
+  const velocidadeNaveVida = 1; // Velocidade de movimento horizontal da nave de vida
+
+  const intervaloMovimento = setInterval(() => {
+    // Converte a posição atual de left de px para número
+    let posicaoAtual = parseInt(naveVida.style.left, 10);
+
+    // Verifica se a nave atingiu os limites laterais da tela
+    if (posicaoAtual >= (larguraCenario - parseInt(naveVida.style.width, 10)) && direcao > 0) {
+      direcao = -1; // Muda a direção para a esquerda
+    } else if (posicaoAtual <= 0 && direcao < 0) {
+      direcao = 1; // Muda a direção para a direita
+    }
+
+    // Atualiza a posição horizontal da nave
+    naveVida.style.left = `${posicaoAtual + velocidadeNaveVida * direcao}px`;
+  }, 20);
+
+  // Guarda o intervalo para poder limpar depois
+  naveVida.dataset.intervalo = intervaloMovimento;
+  intervalosMovimentoNavesVida.push(intervaloMovimento); // Armazena o intervalo
+}
+
+const moverItemVida = (itemVida) => {
+  const intervaloItem = setInterval(() => {
+    let posicaoTopAtual = parseInt(itemVida.style.top, 10);
+    posicaoTopAtual += 3; // Move para baixo
+    itemVida.style.top = `${posicaoTopAtual}px`;
+    if (posicaoTopAtual > alturaCenario) {
+      clearInterval(intervaloItem);
+      itemVida.remove(); // Remove o item se ele sair da tela
+    }
+  }, 30);
+
+  // Guarda o intervalo para poder limpar depois
+  itemVida.dataset.intervalo = intervaloItem;
+  intervalosMovimentoItensVida.push(intervaloItem); // Armazena o intervalo
 }
 
 
-function despausarJogo() {
-  if (jogoPausado) {
-    jogoPausado = false;
-    // Reiniciar os intervalos apenas uma vez para garantir que não haja duplicação
-    checaMoveNave = setInterval(moveNave, 5);
-    checaMoveTiros = setInterval(moveTiros, 50);
-    checaMoveNaveInimigas = setInterval(moveNaveInimigas, 50);
-    checaNaveInimigas = setInterval(naveInimigas, 1000);
-    checaNaveInimigasRapidas = setInterval(naveInimigaRapida, 15000);
-    checaColisao = setInterval(colisao, 10);
-    checaTiros = setInterval(atirar, 10);
-    document.getElementById('botaoPausa').style.display = 'none';
-  }
+
+const destruirNaveVida = (naveVida) => {
+  clearInterval(naveVida.dataset.intervalo); // Para o movimento
+
+  const posicaoHorizontal = parseInt(naveVida.style.left, 10);
+  const posicaoTop = parseInt(naveVida.style.top, 10);
+
+  const larguraNaveVida = 100;  // Largura da nave de vida
+  const alturaNaveVida = 100;   // Altura da nave de vida
+  const larguraExplosao = 50;  // Largura da explosão
+  const alturaExplosao = 50;   // Altura da explosão
+
+  const explosao = document.createElement('div');
+  explosao.className = 'explosao';
+  explosao.style.position = 'absolute';
+  explosao.style.width = `${larguraExplosao}px`;
+  explosao.style.height = `${alturaExplosao}px`;
+  explosao.style.backgroundImage = "url('/imagens/eliminado.gif')";
+  explosao.style.backgroundSize = 'cover';  // Garante que o background cubra todo o elemento
+  explosao.style.left = `${posicaoHorizontal + (larguraNaveVida - larguraExplosao) / 2}px`;
+  explosao.style.top = `${posicaoTop + (alturaNaveVida - alturaExplosao) / 2}px`;
+  cenario.appendChild(explosao);
+
+  setTimeout(() => {
+    explosao.remove();
+  }, 1000);  // Asumindo que a duração da animação é 1000ms
+
+  naveVida.remove(); // Remove do DOM
+  audioExplosoes("/audios/destruido_pod.mp3"); // Toca o som de destruição
+  criarItemVida(`${posicaoHorizontal + (larguraNaveVida - 50) / 2}px`); // Centraliza o item de vida
 }
+
+const criarItemVida = (posicaoHorizontal) => {
+  const itemVida = document.createElement('div');
+  itemVida.className = 'item-vida';
+  itemVida.style.position = 'absolute';
+  itemVida.style.width = '50px';
+  itemVida.style.height = '50px';
+  itemVida.style.backgroundImage = "url('/imagens/vida1.png')";
+  itemVida.style.left = posicaoHorizontal; // Usa a posição horizontal ajustada para centralizar
+  itemVida.style.top = '30px'; // Posição inicial no topo onde o pod foi destruído
+  cenario.appendChild(itemVida);
+  moverItemVida(itemVida);
+}
+
+
+const checarColisaoItemVida = () => {
+  const itensVida = document.querySelectorAll('.item-vida');
+  itensVida.forEach(itemVida => {
+    const rectItemVida = itemVida.getBoundingClientRect();
+    const rectNave = nave.getBoundingClientRect();
+
+    if (rectItemVida.left < rectNave.right &&
+        rectItemVida.right > rectNave.left &&
+        rectItemVida.top < rectNave.bottom &&
+        rectItemVida.bottom > rectNave.top) {
+      // Incrementa a vida do jogador
+      vidaAtual += 1;
+      vida.textContent = `Vida: ${vidaAtual}`;
+
+      // Remove o item de vida do cenário
+      const somVida = new Audio("/audios/vida.mp3");
+      somVida.play();
+      itemVida.remove();
+    }
+  });
+}
+
 
 
 
@@ -473,6 +592,14 @@ const gameOver = () => {
 }
 
 
+
+function abrirModalComandos() {
+  document.getElementById("modalComandos").style.display = "block";
+}
+
+function fecharModalComandos() {
+  document.getElementById("modalComandos").style.display = "none";
+}
 
 function abrirModalEscolhaNave() {
   document.getElementById("modalEscolhaNave").style.display = "block";
@@ -643,6 +770,19 @@ function startAudio() {
 }
 
 
+document.addEventListener('DOMContentLoaded', function() {
+  var tecladoImg = document.getElementById('teclado');
+
+  tecladoImg.addEventListener('mouseover', function() {
+      this.src = './imagens/teclado_marcado.png';
+  });
+
+  tecladoImg.addEventListener('mouseout', function() {
+      this.src = './imagens/teclado.png';
+  });
+});
+
+
 
 const iniciarJogo = () => {
   document.addEventListener("keydown", teclaPressionada);
@@ -658,13 +798,14 @@ const iniciarJogo = () => {
   pontos.textContent = `Pontos: 0`;
   vidaAtual = vidaInicial;
   pontosAtual = pontosIniciais;
-  
+  intervaloCriarNaveVida = setInterval(criarNaveVida, 50000); // Armazena o intervalo
+  intervaloChecarColisaoItemVida = setInterval(checarColisaoItemVida, 50); // Checa a colisão a cada 50ms
   checaMoveNave = setInterval(moveNave, 5);
   checaMoveTiros = setInterval(moveTiros, 50);
-  checaChefe = setInterval(Chefe, 30000)
+  checaChefe = setInterval(Chefe, 30000);
   checaMoveNaveInimigas = setInterval(moveNaveInimigas, 50);
   checaNaveInimigas = setInterval(naveInimigas, 1000);
-  checaNaveInimigasRapidas = setInterval(naveInimigaRapida, 15000);  // Agora chamando corretamente
+  checaNaveInimigasRapidas = setInterval(naveInimigaRapida, 15000);
   checaColisao = setInterval(colisao, 10);
   checaTiros = setInterval(atirar, 10);
   botaoIniciar.style.display = "none";
@@ -676,7 +817,7 @@ const iniciarJogo = () => {
   botaoInimigo.style.display = "none";
   cenario.style.animation = "animarCenario 10s infinite linear";
   audioMenu.pause(); // Pausa a reprodução do áudio
-  audioMenu.currentTime = 0; // Reinicia o áudio para o iníci
+  audioMenu.currentTime = 0; // Reinicia o áudio para o início
   audioJogo.loop = true;
   audioJogo.play();
   // Remover o botão "Game Over" se ele existir no DOM
@@ -686,3 +827,50 @@ const iniciarJogo = () => {
   }
 }
 
+function pausarJogo() {
+  if (!jogoPausado) {
+    jogoPausado = true;
+    // Limpar todos os intervalos para evitar duplicações
+    clearInterval(checaMoveNave);
+    clearInterval(checaMoveTiros);
+    clearInterval(checaMoveNaveInimigas);
+    clearInterval(checaNaveInimigas);
+    clearInterval(checaNaveInimigasRapidas);
+    clearInterval(checaChefe);
+    clearInterval(checaColisao);
+    clearInterval(checaTiros);
+    clearInterval(intervaloCriarNaveVida); // Pausar criação de novas naves de vida
+    clearInterval(intervaloChecarColisaoItemVida); // Pausar checagem de colisão dos itens de vida
+
+    // Limpar os intervalos de movimento das naves de vida e dos itens de vida
+    intervalosMovimentoNavesVida.forEach(intervalo => clearInterval(intervalo));
+    intervalosMovimentoItensVida.forEach(intervalo => clearInterval(intervalo));
+
+    document.getElementById('botaoPausa').style.display = 'block';
+  }
+}
+
+function despausarJogo() {
+  if (jogoPausado) {
+    jogoPausado = false;
+    // Reiniciar os intervalos apenas uma vez para garantir que não haja duplicação
+    checaMoveNave = setInterval(moveNave, 5);
+    checaMoveTiros = setInterval(moveTiros, 50);
+    checaMoveNaveInimigas = setInterval(moveNaveInimigas, 50);
+    checaNaveInimigas = setInterval(naveInimigas, 1000);
+    checaChefe = setInterval(Chefe, 30000);
+    checaNaveInimigasRapidas = setInterval(naveInimigaRapida, 15000);
+    checaColisao = setInterval(colisao, 10);
+    checaTiros = setInterval(atirar, 10);
+    intervaloCriarNaveVida = setInterval(criarNaveVida, 50000); // Reiniciar a criação das naves de vida
+    intervaloChecarColisaoItemVida = setInterval(checarColisaoItemVida, 50); // Reiniciar a checagem de colisão dos itens de vida
+
+    // Reiniciar os intervalos de movimento das naves de vida e dos itens de vida
+    const navesVida = document.querySelectorAll('.nave-vida');
+    navesVida.forEach(naveVida => moverNaveVida(naveVida));
+    const itensVida = document.querySelectorAll('.item-vida');
+    itensVida.forEach(itemVida => moverItemVida(itemVida));
+
+    document.getElementById('botaoPausa').style.display = 'none';
+  }
+}
